967aad13037dc941760f99c3ff45efaf204ced81
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-05-05 18:50:54 +0200

    scss variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: ["src/styles/scss"],
+    prependData: `
+      @import "./helpers/_mixins.scss";
+      @import "./variables/_colors.scss";
+      @import "./variables/_keyframes.scss";
+      @import "./variables/_animations.scss";
+    `,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
