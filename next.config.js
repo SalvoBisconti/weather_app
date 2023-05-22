875fb470c4a900ca683c9f18adef91720cbfc568
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-05-23 00:22:05 +0200

    fixed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: ["src/styles/scss"],
-    prependData: `
-      @import "./helpers/_mixins.scss";
-      @import "./variables/_colors.scss";
-      @import "./variables/_keyframes.scss";
-      @import "./variables/_animations.scss";
-    `,
-  },
+  // sassOptions: {
+  //   includePaths: ["src/styles/scss"],
+  //   prependData: `
+  //     @import "./helpers/_mixins.scss";
+  //     @import "./variables/_colors.scss";
+  //     @import "./variables/_keyframes.scss";
+  //     @import "./variables/_animations.scss";
+  //   `,
+  // },
 };
 
 module.exports = nextConfig;
